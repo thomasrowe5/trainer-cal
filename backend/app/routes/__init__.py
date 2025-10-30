3558da8e8ca1a66d4cb10fa369228fae5a2f12ca
Thomas Rowe <rowetho52@gmail.com>
2025-10-30 17:28:08 -0400

    Disable Google Auth import in routes/__init__.py to fix Render deploy

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from . import stripe_webhook  # type: ignore
-from .auth_google import router as auth_google
+#from .auth_google import router as auth_google
 from .availability import router as availability
 from .bookings import router as bookings
 from .checkout import router as checkout
